210a81debe4cc71651bef8b87cf03ce797535c8e
Dmitry <druzhaev777@gmail.com>
2022-06-08 09:22:41 +0300

    Football
    Teams21-22

---
+++ app/src/main/java/com/example/manual/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.manual;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Создаём массив разделов:
+    private String titles[] = {
+            "00. Real Madrid",
+            "01. Chelsea",
+            "02. Barcelona",
+            "03. Manchester City",
+            "04. Liverpool",
+            "05. Bayern Munich",
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Получим идентификатор ListView
+        ListView listView = findViewById(R.id.listView);
+        //устанавливаем массив в ListView
+        listView.setAdapter(
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, titles));
+        listView.setTextFilterEnabled(true);
+
+        //Обрабатываем щелчки на элементах ListView:
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, DetailActivity.class);
+
+                intent.putExtra("title", position);
+
+                //запускаем вторую активность
+                startActivity(intent);
+            }
+        });
+    }
+}
